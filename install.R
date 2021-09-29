5960c6dab48ac69690de61fd9b6fc2402edc46d7
c-riku <airplanemodeon@gmail.com>
2021-09-29 21:36:20 +0300

    remove install.R

--- install.R
+++
@@ -1,9 +0,0 @@
-install.packages("ggplot2")
-install.packages("plyr")
-install.packages("dplyr")
-install.packages("lme4")
-install.packages("extrafont")
-install.packages("readbulk")
-install.packages("minpack.lm")
-install.packages("GLDEX")
-
